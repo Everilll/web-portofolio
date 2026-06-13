76fe4e745a74eb4553f9fa630c65ca8d57dd1a76
Everilll <averil_mauladani_33rpl@student.smktelkom-mlg.sch.id>
2026-06-13 20:50:32 +0700

    feat(app): implement throttler rate limiting protection

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,10 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AppConfigModule } from './config/config.module';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [PrismaModule, AppConfigModule],
+  imports: [
+    PrismaModule, 
+    AppConfigModule, 
+    AuthModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
