5b66c8ce2ad630f81786ef1ed006488530b5fcb9
Everilll <averil_mauladani_33rpl@student.smktelkom-mlg.sch.id>
2026-06-14 16:27:33 +0700

    docs(common): document pagination dto properties using swagger

--- apps/api/src/common/dto/pagination.dto.ts
+++ apps/api/src/common/dto/pagination.dto.ts
@@ -3,12 +3,25 @@ import { Type } from 'class-transformer';
 import { IsInt, IsOptional, Max, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiPropertyOptional({
+    description: 'Nomor halaman',
+    minimum: 1,
+    default: 1,
+    type: Number,
+  })
   @IsOptional()
   @Type(() => Number)
   @IsInt()
   @Min(1)
   page?: number = 1;
 
+  @ApiPropertyOptional({
+    description: 'Jumlah data per halaman',
+    minimum: 1,
+    maximum: 100,
+    default: 20,
+    type: Number,
+  })
   @IsOptional()
   @Type(() => Number)
   @IsInt()
